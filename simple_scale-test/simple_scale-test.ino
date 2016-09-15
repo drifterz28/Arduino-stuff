074b7efa5e7ab244305937253437c90f3d49a34e
Chris Whitney <cwhitney@goldstar.com>
2016-09-15 10:59:59 -0700

    update reading wieght

--- simple_scale-test/simple_scale-test.ino
+++ simple_scale-test/simple_scale-test.ino
@@ -10,15 +10,18 @@ Q2HX711 hx711_1(hx711_data_pin1, hx711_clock_pin1);
 //Q2HX711 hx711_2(hx711_data_pin2, hx711_clock_pin2);
 
 // ~81808 seems to be 0lbs - subtract value
-int zeroValue = 81808;
+int zeroValue = 0;
 
 void setup() {
   Serial.begin(9600);
+  hx711_1.setScale();
+  hx711_1.tare();
 }
 
 void loop() {
   // built with 3.3v logic
   // ~83886 is the reading when the sensor is not hooked up
+  // ~81808 zero value
   // ~20 container for 2oz
   // ~8 seems to be 2oz
   // ~4 is about 1oz
