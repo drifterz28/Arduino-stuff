a6a5f038d7ac4f30b7d76eaceab4249b9a1add03
Chris Whitney <cwhitney@goldstar.com>
2017-10-17 12:42:01 -0700

    add libs

--- non-blocking-neo/non-blocking-neo.ino
+++ non-blocking-neo/non-blocking-neo.ino
@@ -2,31 +2,41 @@
 // pattern change by push button
 // By Mike Cook Jan 2016
 
-#define PINforControl   0 // pin connected to the small NeoPixels strip
-#define NUMPIXELS1      17 // number of LEDs on strip
-
+#define BUTTON 3
+#define PINforControl 2 // pin connected to the small NeoPixels strip
+#define NUMPIXELS1 189 // number of LEDs on strip
+#include <Button-state.h>
 #include <Adafruit_NeoPixel.h>
+
+Button myBtn(BUTTON, true, true, 20);
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS1, PINforControl, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(NUMPIXELS1, PINforControl, NEO_GRB + NEO_KHZ800);
 
 unsigned long patternInterval = 20 ; // time between steps in the pattern
 unsigned long lastUpdate = 0 ; // for millis() when last update occoured
-unsigned long intervals [] = { 20, 20, 50, 100, 100, 100 } ; // speed for each pattern
+unsigned long intervals [] = { 20, 20, 50, 100, 40, 40, 0 } ; // speed for each pattern
 
-long pattern;
 void setup() {
   strip.begin(); // This initializes the NeoPixel library.
   wipe(); // wipes the LED buffers
-  pattern = random(0, 6);
 }
 
 void loop() {
-  if (millis() - lastUpdate > patternInterval) {
-    updatePattern(pattern);
+  static int pattern = 0, lastReading;
+  myBtn.read();
+  if (myBtn.wasPressed()) {
+    pattern++ ; // change pattern number
+    if (pattern > 7) pattern = 0; // wrap round if too big
+    patternInterval = intervals[pattern]; // set speed for this pattern
+    wipe(); // clear out the buffer
+    delay(50); // debounce delay
   }
+  lastReading = reading; // save for next time
+
+  if (millis() - lastUpdate > patternInterval) updatePattern(pattern);
 }
 
-void  updatePattern(long pat) { // call the pattern currently being created
+void  updatePattern(int pat) { // call the pattern currently being created
   switch (pat) {
     case 0:
       rainbow();
@@ -38,16 +48,19 @@ void  updatePattern(long pat) { // call the pattern currently being created
       theaterChaseRainbow();
       break;
     case 3:
-      colorWipe(strip.Color(255, 0, 0)); // red
-      colorWipe(strip2.Color(255, 0, 0)); // red
+      colorWipe(strip.Color(200, 0, 0)); // red
       break;
     case 4:
-      colorWipe(strip.Color(0, 255, 0)); // green
-      colorWipe(strip2.Color(0, 255, 0)); // green
+      colorWipe(strip.Color(0, 200, 0)); // green
       break;
     case 5:
-      colorWipe(strip.Color(0, 0, 255)); // blue
-      colorWipe(strip2.Color(0, 0, 255)); // blue
+      colorWipe(strip.Color(0, 0, 200)); // blue
+      break;
+    case 6:
+      colorWipe(strip.Color(100, 100, 100)); // ??
+      break;
+    case 7:
+      colorWipe(strip.Color(0, 0, 0)); // off
       break;
   }
 }
@@ -56,10 +69,8 @@ void rainbow() { // modified from Adafruit example to make it a state machine
   static uint16_t j = 0;
   for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, Wheel((i + j) & 255));
-    strip2.setPixelColor(i, Wheel((i + j) & 255));
   }
   strip.show();
-  strip2.show();
   j++;
   if (j >= 256) j = 0;
   lastUpdate = millis(); // time for next change to the display
@@ -69,10 +80,8 @@ void rainbowCycle() { // modified from Adafruit example to make it a state machi
   static uint16_t j = 0;
   for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    strip2.setPixelColor(i, Wheel2(((i * 256 / strip2.numPixels()) + j) & 255));
   }
   strip.show();
-  strip2.show();
   j++;
   if (j >= 256 * 5) j = 0;
   lastUpdate = millis(); // time for next change to the display
@@ -84,18 +93,15 @@ void theaterChaseRainbow() { // modified from Adafruit example to make it a stat
   if (on) {
     for (int i = 0; i < strip.numPixels(); i = i + 3) {
       strip.setPixelColor(i + q, Wheel( (i + j) % 255)); //turn every third pixel on
-      strip2.setPixelColor(i + q, Wheel2( (i + j) % 255)); //turn every third pixel on
     }
   }
   else {
     for (int i = 0; i < strip.numPixels(); i = i + 3) {
       strip.setPixelColor(i + q, 0);      //turn every third pixel off
-      strip2.setPixelColor(i + q, 0);      //turn every third pixel off
     }
   }
   on = !on; // toggel pixelse on or off for next time
   strip.show(); // display
-  strip2.show();
   q++; // update the q variable
   if (q >= 3 ) { // if it overflows reset it and update the J variable
     q = 0;
@@ -108,9 +114,7 @@ void theaterChaseRainbow() { // modified from Adafruit example to make it a stat
 void colorWipe(uint32_t c) { // modified from Adafruit example to make it a state machine
   static int i = 0;
   strip.setPixelColor(i, c);
-  strip2.setPixelColor(i, c);
   strip.show();
-  strip2.show();
   i++;
   if (i >= strip.numPixels()) {
     i = 0;
@@ -123,21 +127,7 @@ void colorWipe(uint32_t c) { // modified from Adafruit example to make it a stat
 void wipe() { // clear all LEDs
   for (int i = 0; i < strip.numPixels(); i++) {
     strip.setPixelColor(i, strip.Color(0, 0, 0));
-    strip2.setPixelColor(i, strip.Color(0, 0, 0));
   }
-}
-
-uint32_t Wheel2(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip2.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip2.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip2.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
 uint32_t Wheel(byte WheelPos) {
